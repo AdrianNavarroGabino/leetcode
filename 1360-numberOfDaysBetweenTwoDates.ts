fef34b38698f2be7e5ce9142c6856c35a0f287bb
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2025-03-19 07:49:14 +0100

    1360 - Number of Days Between Two Dates

---
+++ 1360-numberOfDaysBetweenTwoDates.ts
@@ -0,0 +1,6 @@
+function daysBetweenDates(date1: string, date2: string): number {
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+
+    return Math.abs(d2.getTime() - d1.getTime()) / 86400000;
+};
